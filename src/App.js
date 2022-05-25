ac659d8a71d7ed5652ad7145acbaf00953f1ce5d
Daniel Batista <dani_batista20@hotmail.com>
2022-05-26 00:22:29 +0100

    start: features/weather - useGeoLocation()

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React from 'react'
 import Layout from 'components/layout/Layout'
-import { Row, Col } from 'react-bootstrap'
+import Weather from 'features/weather'
+// import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 // import logo from './logo.svg'
 import 'styles/globals.scss'
 
 function App() {
   return (
     <Layout>
-      <Row>
-        <Col md={6} className="mx-auto">
-          <h1>Hello</h1>
-        </Col>
-      </Row>
+      <Weather />
     </Layout>
   )
 }
