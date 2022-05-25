d4a49e17837c6bef5260a6b6606755998bd9df6f
Daniel Batista <dani_batista20@hotmail.com>
2022-05-26 00:22:29 +0100

    start: features/weather - useGeoLocation()

--- src/features/weather/index.js
+++ src/features/weather/index.js
@@ -1,60 +1,36 @@
-import { Fragment, useState } from 'react'
-import { useGetInitialCityQuery } from '../api/apiSlice'
+import { Fragment, useState, useEffect } from 'react'
 // import { Container, Row, Col, Form, Button } from 'react-bootstrap'
-import Search from './Search'
-import 'assets/owfont-master/css/owfont-regular.css'
+// import Search from './Search'
 import useGeoLocation from 'hooks/useGeolocation'
+// import 'assets/owfont-master/css/owfont-regular.css'
 
 const Weather = () => {
-  //
+  // Get Location
   const location = useGeoLocation()
-  const [initialCity, setInitialCity] = useState(
-    `${location.loaded === true ? location.coordinates : 'leiria'}`
-  )
-  const [newCity, setNewCity] = useState('leiria')
-  console.log(location)
+  // First City
+  const [initialCity, setInitialCity] = useState({
+    lat: 39.7436,
+    lon: -8.8071
+  })
 
-  const {
-    data: city,
-    isLoading,
-    isSuccess,
-    isError,
-    error
-  } = useGetInitialCityQuery(newCity)
-
-  const searchLocation = (event) => {
-    if (event.key === 'Enter') {
+  // useEffect
+  useEffect(() => {
+    if (location.loaded && !location.error) {
+      setInitialCity({
+        lat: location.coordinates.lat,
+        lon: location.coordinates.lon
+      })
     }
-  }
+    // return () => {console.log('clear')}
+  }, [location])
 
-  let content
-  if (isLoading) {
-    content = <p>Loading...</p>
-  } else if (isSuccess) {
-    // console.log(city)
-    content = (
-      <article>
-        <div className="local">
-          <h1>{city.name}</h1>
-        </div>
-        <div className="temp">
-          <h2>
-            {Math.round(city.main.temp)} <sup>º</sup>C
-          </h2>
-          <h3>{city.weather[0].main}</h3>
-          <h3>
-            <i className={`owf owf-${city.weather[0].id} owf-5x`}></i>
-          </h3>
-        </div>
-      </article>
-    )
-  } else if (isError) {
-    content = <p>{error}</p>
-  }
+  console.log('->', initialCity);
+
+
+  let content = <p>content</p>
 
   return (
     <Fragment>
-      <Search />
       <section className="">{content}</section>
     </Fragment>
   )
