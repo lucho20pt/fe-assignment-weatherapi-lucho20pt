aca1cf71c4b1e7a698f56174e96c7b5d355ada2f
Daniel Batista <dani_batista20@hotmail.com>
2022-05-26 01:47:25 +0100

    upd: features/weather - add: Loading component

--- src/features/weather/index.js
+++ src/features/weather/index.js
@@ -1,6 +1,7 @@
 import { Fragment, useState, useEffect } from 'react'
 import { useGetCityByCoordsQuery } from '../api/weatherApi'
-// import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import Loading from 'components/ui/Loading'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 // import Search from './Search'
 import useGeoLocation from 'hooks/useGeolocation'
 import 'assets/owfont-master/css/owfont-regular.css'
@@ -35,26 +36,29 @@ const Weather = () => {
     error
   } = useGetCityByCoordsQuery(initialCity)
 
-
   // let content = <p>content</p>
   let content = <h1>{city && city.name}</h1>
   if (isLoading) {
-    content = <p>Loading...</p>
+    content = <Loading />
   } else if (isSuccess) {
     // console.log(city)
     content = (
       <article>
-        <div className="local">
+        <Row>
+          <Col className="weather | text-center">
+            <i className={`owf owf-${city.weather[0].id} owf-5x`}></i>
+          </Col>
+        </Row>
+
+        <div className="name">
           <h1>{city.name}</h1>
         </div>
         <div className="temp">
           <h2>
-            {Math.round(city.main.temp)} <sup>º</sup>C
+            {Math.round(city.main.temp)}
+            <sup>ºC</sup>
           </h2>
           <h3>{city.weather[0].main}</h3>
-          <h3>
-            <i className={`owf owf-${city.weather[0].id} owf-5x`}></i>
-          </h3>
         </div>
       </article>
     )
@@ -65,7 +69,9 @@ const Weather = () => {
   return (
     <Fragment>
       {/* <Search /> */}
-      <section className="">{content}</section>
+      <section className="">
+        <Container>{content}</Container>
+      </section>
     </Fragment>
   )
 }
