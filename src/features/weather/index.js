b3f09dba884cc8f008cd00316c1218cd1246d2c7
Daniel Batista <dani_batista20@hotmail.com>
2022-05-26 01:25:36 +0100

    upd: features/weather - render data

--- src/features/weather/index.js
+++ src/features/weather/index.js
@@ -3,7 +3,7 @@ import { useGetCityByCoordsQuery } from '../api/weatherApi'
 // import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 // import Search from './Search'
 import useGeoLocation from 'hooks/useGeolocation'
-// import 'assets/owfont-master/css/owfont-regular.css'
+import 'assets/owfont-master/css/owfont-regular.css'
 
 const Weather = () => {
   // Get Location
@@ -38,29 +38,29 @@ const Weather = () => {
 
   // let content = <p>content</p>
   let content = <h1>{city && city.name}</h1>
-  // if (isLoading) {
-  //   content = <p>Loading...</p>
-  // } else if (isSuccess) {
-  //   // console.log(city)
-  //   content = (
-  //     <article>
-  //       <div className="local">
-  //         <h1>{city.name}</h1>
-  //       </div>
-  //       <div className="temp">
-  //         <h2>
-  //           {Math.round(city.main.temp)} <sup>º</sup>C
-  //         </h2>
-  //         <h3>{city.weather[0].main}</h3>
-  //         <h3>
-  //           <i className={`owf owf-${city.weather[0].id} owf-5x`}></i>
-  //         </h3>
-  //       </div>
-  //     </article>
-  //   )
-  // } else if (isError) {
-  //   content = <p>{error}</p>
-  // }
+  if (isLoading) {
+    content = <p>Loading...</p>
+  } else if (isSuccess) {
+    // console.log(city)
+    content = (
+      <article>
+        <div className="local">
+          <h1>{city.name}</h1>
+        </div>
+        <div className="temp">
+          <h2>
+            {Math.round(city.main.temp)} <sup>º</sup>C
+          </h2>
+          <h3>{city.weather[0].main}</h3>
+          <h3>
+            <i className={`owf owf-${city.weather[0].id} owf-5x`}></i>
+          </h3>
+        </div>
+      </article>
+    )
+  } else if (isError) {
+    content = <p>{error}</p>
+  }
 
   return (
     <Fragment>
