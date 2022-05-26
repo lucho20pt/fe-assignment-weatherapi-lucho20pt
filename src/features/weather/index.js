8384672b7979c2fe924537ec7d392cc89c18ee65
Daniel Batista <dani_batista20@hotmail.com>
2022-05-26 01:08:18 +0100

    upd: features/weather - useGetCityByCoordsQuery()

--- src/features/weather/index.js
+++ src/features/weather/index.js
@@ -1,4 +1,5 @@
 import { Fragment, useState, useEffect } from 'react'
+import { useGetCityByCoordsQuery } from '../api/weatherApi'
 // import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 // import Search from './Search'
 import useGeoLocation from 'hooks/useGeolocation'
@@ -24,13 +25,46 @@ const Weather = () => {
     // return () => {console.log('clear')}
   }, [location])
 
-  console.log('->', initialCity);
+  // console.log('->', initialCity);
+
+  const {
+    data: city,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetCityByCoordsQuery(initialCity)
 
 
-  let content = <p>content</p>
+  // let content = <p>content</p>
+  let content = <h1>{city && city.name}</h1>
+  // if (isLoading) {
+  //   content = <p>Loading...</p>
+  // } else if (isSuccess) {
+  //   // console.log(city)
+  //   content = (
+  //     <article>
+  //       <div className="local">
+  //         <h1>{city.name}</h1>
+  //       </div>
+  //       <div className="temp">
+  //         <h2>
+  //           {Math.round(city.main.temp)} <sup>º</sup>C
+  //         </h2>
+  //         <h3>{city.weather[0].main}</h3>
+  //         <h3>
+  //           <i className={`owf owf-${city.weather[0].id} owf-5x`}></i>
+  //         </h3>
+  //       </div>
+  //     </article>
+  //   )
+  // } else if (isError) {
+  //   content = <p>{error}</p>
+  // }
 
   return (
     <Fragment>
+      {/* <Search /> */}
       <section className="">{content}</section>
     </Fragment>
   )
