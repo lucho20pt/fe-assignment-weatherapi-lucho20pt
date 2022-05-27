df71480848eace8db749cd968f2c3b65ae4e6169
Daniel Batista <dani_batista20@hotmail.com>
2022-05-27 08:59:45 +0100

    upd: features/weather/ & add: features/weather/forecast & hooks/useGetDayName testing

--- src/features/weather/index.js
+++ src/features/weather/index.js
@@ -1,10 +1,11 @@
 import { Fragment, useState, useEffect } from 'react'
 import { useGetCityByCoordsQuery } from '../api/weatherApi'
-import Loading from 'components/ui/Loading'
+import useGeoLocation from 'hooks/useGeolocation'
 // import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import { Container, Row, Col } from 'react-bootstrap'
 import Search from './Search'
-import useGeoLocation from 'hooks/useGeolocation'
+import Forecast from './Forecast'
+import Loading from 'components/ui/Loading'
 import 'assets/owfont-master/css/owfont-regular.css'
 import classes from 'styles/features/weather/weather.module.scss'
 
@@ -16,6 +17,22 @@ const Weather = () => {
     lat: 39.74362,
     lon: -8.80705
   })
+  const [savedCitysList, setSavedCitysList] = useState([])
+  // useEffect(() => {
+  //   const citys = JSON.parse(localStorage.getItem('citys'))
+  //   if (citys) {
+  //     setSavedCitysList(citys)
+  //   }
+  // }, [])
+
+  // weather api
+  const {
+    data: city,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetCityByCoordsQuery(initialCity)
 
   // useEffect
   useEffect(() => {
@@ -25,52 +42,47 @@ const Weather = () => {
         lon: location.coordinates.lon
       })
     }
+    city && setSavedCitysList([city])
     // return () => {console.log('clear')}
-  }, [location])
+  }, [location, city])
+  // localStorage.setItem('citys', JSON.stringify(savedCitysList))
 
-  // console.log('->', initialCity);
-
-  const {
-    data: city,
-    isLoading,
-    isSuccess,
-    isError,
-    error
-  } = useGetCityByCoordsQuery(initialCity)
-
-  // let content = <p>content</p>
+  // content RENDER condition
   let content
   if (isLoading) {
     content = <Loading />
   } else if (isSuccess) {
-    // console.log(city)
-    content = (
-      <article className={`${classes.weather} p-4`}>
-        <Row>
-          <Col className="mx-auto text-center">
-            <div className={`${classes.icon}`}>
-              <i className={`owf owf-${city.weather[0].id} owf-5x`}></i>
-            </div>
-            <div className={`${classes.name}`}>
-              <h1>{city.name}</h1>
-            </div>
-            <Row>
-              <Col className={`${classes.temp}`}>
-                <h2>
-                  {Math.round(city.main.temp)}
-                  <sup>ºC</sup>
-                </h2>
-              </Col>
-              <h3>{city.weather[0].main}</h3>
-            </Row>
-          </Col>
-        </Row>
-      </article>
-    )
+    content = savedCitysList.map((city, i) => (
+      <Fragment key={i}>
+        <article className={`${classes.weather} p-4`}>
+          <Row>
+            <Col className="mx-auto text-center">
+              <div className={`${classes.icon}`}>
+                <i className={`owf owf-${city.weather[0].id} owf-5x`}></i>
+              </div>
+              <div className={`${classes.name}`}>
+                <h1>{city.name}</h1>
+              </div>
+              <Row>
+                <Col className={`${classes.temp}`}>
+                  <h2>
+                    {Math.round(city.main.temp)}
+                    <sup>ºC</sup>
+                  </h2>
+                </Col>
+                <h3>{city.weather[0].main}</h3>
+              </Row>
+            </Col>
+          </Row>
+        </article>
+        <Forecast name={city.name} />
+      </Fragment>
+    ))
   } else if (isError) {
     content = <p>{error}</p>
   }
 
+  // component RETURN
   return (
     <Fragment>
       <section className="">
