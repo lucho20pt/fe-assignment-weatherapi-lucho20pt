0c4d3e65a936955482f42e09270b3ed2bd581dd8
Daniel Batista <dani_batista20@hotmail.com>
2022-05-27 09:03:45 +0100

    upd: hooks/useGetDayName

--- src/hooks/useGetDayName.js
+++ src/hooks/useGetDayName.js
@@ -1,8 +1,13 @@
-import { useState, useEffect } from 'react'
-
 const useGetDayName = (dt, timezone) => {
-
-  const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ]
   const date = new Date(dt * 1000 + timezone * 1000).getDay()
   return weekDays[date]
 }
