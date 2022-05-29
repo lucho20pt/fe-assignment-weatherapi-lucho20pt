9005e458a665ba1f04461b5b8bebbf9b19e913bf
Daniel Batista <dani_batista20@hotmail.com>
2022-05-29 18:41:25 +0100

    upd: Search - useGetCityByNameMutation()

--- src/features/weather/Search.js
+++ src/features/weather/Search.js
@@ -1,10 +1,12 @@
 import { Fragment, useState } from 'react'
+import { useGetCityByNameMutation } from 'features/api/weatherApi'
 import { Form, Button } from 'react-bootstrap'
 
 const Search = () => {
   //
   const [showSearch, setShowSearch] = useState(false)
   const [search, setSearch] = useState('')
+  const [getCityByName] = useGetCityByNameMutation()
 
   // show search
   const showSearchHandler = () => {
@@ -19,56 +21,68 @@ const Search = () => {
     setSearch(val)
   }
   // on submit
-  const submitHandler = async (event, data) => {
+  const submitHandler = async (event) => {
     event.preventDefault()
     console.log('submit ->', search)
 
-    // clear form
-    setSearch('')
-    setShowSearch(!showSearch)
+    // not empty field
+    if (search === '') {
+      return true
+    }
+
+    // request / result / verify
+    try {
+      // get response
+      const cityResponse = await getCityByName(search).unwrap()
+      console.log(cityResponse)
+    } catch (error) {
+      console.log('error -> ', error.status)
+      console.log('error -> ', error.data.message)
+    }
   }
 
   return (
-    <header className="search | my-3 d-flex justify-content-between">
-      {/* {cityResponse && cityResponse.name} */}
-      {!showSearch && (
-        <Button
-          className=""
-          size="lg"
-          variant="outline-primary"
-          onClick={showSearchHandler}
-        >
-          Add City
-        </Button>
-      )}
-
-      {showSearch && (
-        <Fragment>
-          <Form
-            className="w-100 d-flex justify-content-between"
-            onSubmit={submitHandler}
+    <Fragment>
+      <header className="search | my-3 d-flex justify-content-between | position-relative">
+        {!showSearch && (
+          <Button
+            className=""
+            size="lg"
+            variant="outline-primary"
+            onClick={showSearchHandler}
           >
-            <Button
-              type="submit"
-              className=""
-              size="lg"
-              variant="outline-primary"
-            >
-              +
-            </Button>
+            Add City
+          </Button>
+        )}
 
-            <Form.Control
-              size="lg"
-              className="w-75"
-              type="text"
-              placeholder="Enter Location..."
-              value={search}
-              onChange={changeHandler}
-            />
-          </Form>
-        </Fragment>
-      )}
-    </header>
+        {showSearch && (
+          <Fragment>
+            <Form
+              className="w-100 d-flex justify-content-between"
+              onSubmit={submitHandler}
+            >
+              <Button
+                type="submit"
+                className=""
+                size="lg"
+                variant="outline-primary"
+              >
+                +
+              </Button>
+
+              <Form.Control
+                size="lg"
+                className="w-75"
+                type="text"
+                placeholder="Enter Location..."
+                value={search}
+                onChange={changeHandler}
+              />
+            </Form>
+          </Fragment>
+        )}
+      </header>
+    </Fragment>
   )
 }
 
