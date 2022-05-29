fcccdf8e5b0da011327701e163d67468353829ba
Daniel Batista <dani_batista20@hotmail.com>
2022-05-29 13:28:06 +0100

    add: Slider - test

--- src/features/weather/index.js
+++ src/features/weather/index.js
@@ -1,16 +1,25 @@
 import { Fragment } from 'react'
 import { Container } from 'react-bootstrap'
-import Search from './Search'
+import Slider from 'components/layout/Slider'
+import Carousel from 'react-bootstrap/Carousel'
+import Search from 'features/weather/Search'
 import WeatherLocal from 'features/weather/WeatherLocal'
 
 const Weather = () => {
   // component RETURN
   return (
     <Fragment>
-      <section className="">
+      <section>
         <Container style={{ maxWidth: '500px' }}>
           <Search />
-          <WeatherLocal />
+          <Slider>
+            <Carousel.Item>
+              <WeatherLocal />
+            </Carousel.Item>
+            <Carousel.Item>
+              <WeatherLocal />
+            </Carousel.Item>
+          </Slider>
         </Container>
       </section>
     </Fragment>
