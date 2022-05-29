22bd5ca47c0745267308387ea9651b218f93a0fd
Daniel Batista <dani_batista20@hotmail.com>
2022-05-29 17:16:22 +0100

    upd: weatherApi - getCityByName, useGetCityByName

--- src/features/api/weatherApi.js
+++ src/features/api/weatherApi.js
@@ -18,26 +18,26 @@ export const weatherApi = createApi({
         // api.openweathermap.org/data/2.5/onecall?APPID=90670e234808f3e0060efb18c2a77546&lat=39.7436&lon=-8.8071&exclude=hourly,minutely&units=metric
         `data/2.5/onecall?APPID=${process.env.REACT_APP_APPID}&lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric`,
       providesTags: ['City']
-    })
-    // getCity: builder.mutation({
-    //   query: ({ lat, lon }) => ({
-    //     // api.openweathermap.org/data/2.5/weather?APPID=90670e234808f3e0060efb18c2a77546&lat=39.7436&lon=-8.8071&units=metric
-    //     url: `api.openweathermap.org/data/2.5/weather?APPID=${process.env.REACT_APP_APPID}&lat=${lat}&lon=${lon}&units=metric`
-    //   }),
-    //   invalidatesTags: ['City']
-    // }),
+    }),
     // getCityByName: builder.query({
     //   query: (city) =>
     //     // api.openweathermap.org/data/2.5/weather?APPID=90670e234808f3e0060efb18c2a77546&q=leiria&units=metric
-    //     // transformResponse: res => res.sort((a, b) => b.id - a.id),
-    //     `data/2.5/weather?APPID=${process.env.REACT_APP_APPID}&q=${city}&units=metric`,
+    //     `data/2.5/weather?APPID=${process.env.REACT_APP_APPID}q=${city}&units=metric`,
     //   providesTags: ['City']
-    // })
+    // }),
+    getCityByName: builder.mutation({
+      query: (city) => ({
+        // api.openweathermap.org/data/2.5/weather?APPID=90670e234808f3e0060efb18c2a77546&q=leiria&units=metric
+        url: `data/2.5/weather?APPID=${process.env.REACT_APP_APPID}&q=${city}&units=metric`,
+        method: 'GET'
+      })
+    })
   })
 })
 
 export const {
   useGetCityByCoordsQuery,
-  useGetCityByCoords7DaysQuery
-  // useGetCityByNameQuery
+  useGetCityByCoords7DaysQuery,
+  // useGetCityByNameQuery,
+  useGetCityByNameMutation
 } = weatherApi
