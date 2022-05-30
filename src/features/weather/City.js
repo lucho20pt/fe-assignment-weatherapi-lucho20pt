51ad51dcc069bac6f073871e0d62ff8937b6f1c3
Daniel Batista <dani_batista20@hotmail.com>
2022-05-30 01:52:55 +0100

    add: City component

---
+++ src/features/weather/City.js
@@ -0,0 +1,57 @@
+import { Fragment, useState } from 'react'
+import moment from 'moment'
+import { Row, Col, Button } from 'react-bootstrap'
+import Forecast from './Forecast'
+import 'assets/owfont-master/css/owfont-regular.css'
+import classes from 'styles/features/weather/weather.module.scss'
+
+const City = ({ city }) => {
+  const [showForecast, setShowForecast] = useState(false)
+
+  const showForecastHandler = () => {
+    setShowForecast(!showForecast)
+  }
+
+  // content RENDER
+  return (
+    <Fragment key={city.id}>
+      <article className={`${classes.weather} | p-2 mb-2 | position-relative`}>
+        <Row>
+          <Col className="mx-auto text-center">
+            <div className={`${classes.icon}`}>
+              <i className={`owf owf-${city.weather[0].id} owf-5x`}></i>
+            </div>
+
+            <h1 className={`${classes.name}`}>
+              {city.name} / {city.sys.country}
+            </h1>
+
+            <h2 className={`${classes.temp}`}>
+              {Math.round(city.main.temp)}
+              <sup>ºC</sup>
+            </h2>
+
+            <h3>{city.weather[0].main}</h3>
+
+            <small>{moment.unix(city.dt).format('dddd / D MMMM')}</small>
+          </Col>
+        </Row>
+      </article>
+      {/* <aside className="text-center">
+        {!showForecast && (
+          <Button
+            className="mx-auto"
+            size="lg"
+            variant="outline-primary"
+            onClick={showForecastHandler}
+          >
+            Forecast for 7 days
+          </Button>
+        )}
+        {showForecast && <Forecast coords={initialCity} />}
+      </aside> */}
+    </Fragment>
+  )
+}
+
+export default City
