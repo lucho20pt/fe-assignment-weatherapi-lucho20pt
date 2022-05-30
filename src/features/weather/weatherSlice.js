bdc3c20f5e790fa423c0087ffd7d3c6cf17eeca5
Daniel Batista <dani_batista20@hotmail.com>
2022-05-30 20:07:10 +0100

    upd: weatherSlice - removeCity()

--- src/features/weather/weatherSlice.js
+++ src/features/weather/weatherSlice.js
@@ -1,40 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialWheaterState = {
-//   citys: [
-//     {
-//       coord: { lon: -8.8071, lat: 39.7436 },
-//       weather: [
-//         { id: 803, main: 'Clouds', description: 'broken clouds', icon: '04d' }
-//       ],
-//       base: 'stations',
-//       main: {
-//         temp: 19.57,
-//         feels_like: 19.31,
-//         temp_min: 17.72,
-//         temp_max: 21.53,
-//         pressure: 1009,
-//         humidity: 66
-//       },
-//       visibility: 10000,
-//       wind: { speed: 4.63, deg: 250 },
-//       clouds: { all: 75 },
-//       dt: 1653848768,
-//       sys: {
-//         type: 2,
-//         id: 2011333,
-//         country: 'PT',
-//         sunrise: 1653801055,
-//         sunset: 1653854080
-//       },
-//       timezone: 3600,
-//       id: 2267095,
-//       name: 'Leiria',
-//       cod: 200
-//     }
-//   ],
+  //   citys: [
+  //     {
+  //       coord: { lon: -8.8071, lat: 39.7436 },
+  //       weather: [
+  //         { id: 803, main: 'Clouds', description: 'broken clouds', icon: '04d' }
+  //       ],
+  //       base: 'stations',
+  //       main: {
+  //         temp: 19.57,
+  //         feels_like: 19.31,
+  //         temp_min: 17.72,
+  //         temp_max: 21.53,
+  //         pressure: 1009,
+  //         humidity: 66
+  //       },
+  //       visibility: 10000,
+  //       wind: { speed: 4.63, deg: 250 },
+  //       clouds: { all: 75 },
+  //       dt: 1653848768,
+  //       sys: {
+  //         type: 2,
+  //         id: 2011333,
+  //         country: 'PT',
+  //         sunrise: 1653801055,
+  //         sunset: 1653854080
+  //       },
+  //       timezone: 3600,
+  //       id: 2267095,
+  //       name: 'Leiria',
+  //       cod: 200
+  //     }
+  //   ],
   citys: [],
-  changed: false,
+  changed: false
 }
 
 const weatherSlice = createSlice({
@@ -44,24 +44,20 @@ const weatherSlice = createSlice({
     addCity(state, action) {
       console.log('addCity')
       const newCity = action.payload
-      const existingCity = state.citys.find(
-        (city) => city.id === newCity.id
-      )
+      const existingCity = state.citys.find((city) => city.id === newCity.id)
       state.changed = true
       if (!existingCity) {
         state.citys.unshift(newCity)
       }
-      //   if (!existingCity) {
-      //     state.citys.push({
-      //       id: existingCity.sys.id,
-      //     });
-      //   } else {
-      //     state.changed = false;
-      //     return false
-      //   }
     },
-    removeCity(state) {
+    removeCity(state, action) {
       console.log('removeCity')
+      const id = action.payload
+      console.log(id)
+      const existingCity = state.citys.find((city) => city.id === id)
+      if (existingCity) {
+        state.citys = state.citys.filter((city) => city.id !== id)
+      }
     }
   }
 })
