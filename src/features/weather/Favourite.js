8c9e85957db0b12235ce2ea254e3a2c161868621
Daniel Batista <dani_batista20@hotmail.com>
2022-05-30 20:06:35 +0100

    upd: Favourite - useDispatch(), deleteCityHandler()

--- src/features/weather/Favourite.js
+++ src/features/weather/Favourite.js
@@ -1,12 +1,18 @@
 import { Fragment } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-// import { weatherActions } from 'features/weather/weatherSlice'
+import { weatherActions } from 'features/weather/weatherSlice'
 import classes from 'styles/features/weather/favourite.module.scss'
 
 import { Container, Row, Col, Button } from 'react-bootstrap'
 
 const Favourite = (props) => {
   const favourite = useSelector((state) => state.weather.citys)
+  const dispatch = useDispatch()
+
+  const deleteCityHandler = (id) => { 
+    console.log(id);
+    dispatch(weatherActions.removeCity(id))
+  }
 
   // content RENDER condition
   let content
@@ -14,7 +20,7 @@ const Favourite = (props) => {
     content = <p>You have no Favourite Citys</p>
   } else {
     content = favourite.map((city) => (
-      <Row
+      <Row key={city.id}
         className={`${classes.favourite} | align-items-center justify-content-between | py-2 px-3 mb-3`}
       >
         <Col xs={6} sm={7} className={classes.city}>
@@ -22,7 +28,9 @@ const Favourite = (props) => {
         </Col>
         <Col className={classes.temp}>{city.main.temp.toFixed(0)}º</Col>
         <Col className="text-end">
-          <Button size="sm" variant="outline-warning" title="Remove City">
+          <Button size="sm" variant="outline-warning" title="Remove City"
+            onClick={()=> deleteCityHandler(city.id)}
+          >
             x
           </Button>
         </Col>
