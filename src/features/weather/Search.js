50651a8658d263a43737ad427a973a2002c8b837
Daniel Batista <dani_batista20@hotmail.com>
2022-05-30 02:27:47 +0100

    upd: globals - styles && Search - set autofocus

--- src/features/weather/Search.js
+++ src/features/weather/Search.js
@@ -91,6 +91,7 @@ const Search = () => {
               placeholder="Enter Location..."
               value={search}
               onChange={changeHandler}
+              autoFocus
             />
           </Form>
         )}
