c7f0ac502eb5456c8bd39da6ed33ed949822beda
Daniel Batista <dani_batista20@hotmail.com>
2022-05-30 17:34:49 +0100

    upd: Favourite - styles and rendered layout

--- src/features/weather/Favourite.js
+++ src/features/weather/Favourite.js
@@ -1,9 +1,43 @@
 import { Fragment } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { weatherActions } from 'features/weather/weatherSlice'
+import classes from 'styles/features/weather/favourite.module.scss'
+
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 
 const Favourite = (props) => {
+  const Favourite = useSelector((state) => state.weather.citys)
+
   return (
     <Fragment>
-      <h1>hello fav</h1>
+      <Container>
+        <Row
+          className={`${classes.favourite} | align-items-center justify-content-between | py-2 px-3 mb-3`}
+        >
+          <Col>
+            Porto / <span>PT</span>
+          </Col>
+          <Col>22 ºC</Col>
+          <Col className="text-end">
+            <Button className="" variant="outline-warning" title="Remove City">
+              x
+            </Button>
+          </Col>
+        </Row>
+        <Row
+          className={`${classes.favourite} | align-items-center justify-content-between | py-2 px-3 mb-3`}
+        >
+          <Col>
+            Lisboa / <span>PT</span>
+          </Col>
+          <Col>22 ºC</Col>
+          <Col className="text-end">
+            <Button className="" variant="outline-warning" title="Remove City">
+              x
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </Fragment>
   )
 }
